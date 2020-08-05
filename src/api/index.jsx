14483144e629430d28da1ced758a9ed01db9ef0d
Marcel Schmager <schmager29@gmail.com>
2020-08-05 11:05:18 +0200

    Added Posts from API

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,73 +1,31 @@
 import axios from 'axios';
 
-const url = 'http://api.marcel-schmager.com/api/sendMail';
-const posts = [
-  {
-    id: 1,
-    post_title: 'Project One',
-    post_shortext:
-      '<p>Lorem ipsum dolor sit amet,</p> consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    post_heroimage: '/static/media/its_me.8113bbd2.jpg',
-    post_github: 'https://github.com/Tobeje',
-    post_tags: ['React', 'PHP', 'CSS'],
-  },
-  {
-    id: 2,
-    post_title: 'Project two',
-    post_shortext:
-      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    post_heroimage: '/static/media/its_me.8113bbd2.jpg',
-    post_github: 'https://github.com/Tobeje',
-    post_tags: ['React', 'PHP', 'CSS'],
-  },
-  {
-    id: 3,
-    post_title: 'Project three',
-    post_shortext:
-      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    post_heroimage: '/static/media/its_me.8113bbd2.jpg',
-    post_github: 'https://github.com/Tobeje',
-    post_tags: ['React', 'PHP', 'CSS'],
-  },
-  {
-    id: 4,
-    post_title: 'Project four',
-    post_shortext:
-      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    post_heroimage: '/static/media/its_me.8113bbd2.jpg',
-    post_github: 'https://github.com/Tobeje',
-    post_tags: ['React', 'PHP', 'CSS'],
-  },
-  {
-    id: 5,
-    post_title: 'Project five',
-    post_shortext:
-      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-    post_heroimage: '/static/media/its_me.8113bbd2.jpg',
-    post_github: 'https://github.com/Tobeje',
-    post_tags: ['React', 'PHP', 'CSS'],
-  },
-];
+const url = 'http://api.marcel-schmager.com/api/';
 
 export const sendMail = (data) => {
-  axios({
-    method: 'post',
-    body: JSON.stringify(data),
-    url: `${url}`,
-    header: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    data,
-  })
-    .then((result) => {
-      console.log(result.data.sent);
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+	axios({
+		method: 'post',
+		body: JSON.stringify(data),
+		url: `${url}sendMail`,
+		header: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		data
+	})
+		.then((result) => {
+			console.log(result.data.sent);
+		})
+		.catch((error) => {
+			console.log(error.message);
+		});
 };
 
 export const getPosts = async () => {
-  return posts;
+	try {
+		const { data } = await axios.get(`${url}posts`);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 };
--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,90 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Grid,
-  Typography,
-  Card,
-  CardContent,
-  Divider,
-  Button,
-  styled,
-  CircularProgress,
-  Chip,
+	Grid,
+	Typography,
+	Card,
+	CardContent,
+	Divider,
+	Button,
+	styled,
+	CircularProgress,
+	Chip
 } from '@material-ui/core';
 import { getPosts } from '../../api';
 import styles from './ProjectList.module.css';
 
 const MyButton = styled(Button)({
-  background: 'linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)',
-  border: 0,
-  borderRadius: 3,
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, 0.3)',
-  color: 'white',
-  height: 48,
-  padding: '0 30px',
-  margin: '30px 10px',
+	background: 'linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)',
+	border: 0,
+	borderRadius: 3,
+	boxShadow: '0 3px 5px 2px rgba(255, 105, 135, 0.3)',
+	color: 'white',
+	height: 48,
+	padding: '0 30px',
+	margin: '30px 10px'
 });
 const ProjectList = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const fetchAPI = async () => {
-      setPosts(await getPosts());
-    };
+	const [ posts, setPosts ] = useState([]);
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setPosts(await getPosts());
+		};
 
-    fetchAPI();
-  }, []);
+		fetchAPI();
+	}, []);
 
-  if (posts.length === 0) {
-    return (
-      <div className={styles.loading}>
-        <CircularProgress />
-      </div>
-    );
-  }
-  return (
-    <div className={styles.items}>
-      <div className={styles.container}>
-        <Grid
-          container
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-        >
-          {posts.map((post) => (
-            <Grid
-              item
-              xs={12}
-              md={5}
-              component={Card}
-              key={post.id}
-              className={styles.card}
-            >
-              <CardContent>
-                <Typography variant="h2">{post.post_title}</Typography>
-                <Divider component="li" />
-                <img
-                  className={styles.sectionimg}
-                  src={post.post_heroimage}
-                  alt="postimage"
-                ></img>
-                <Typography variant="body2" align="justify">
-                  {post.post_shortext}
-                </Typography>
-                <MyButton href={`post/${post.id}`}>Read more...</MyButton>
-                <MyButton href={post.post_github} target="noopener">
-                  Github
-                </MyButton>
-                <div>
-                  {post.post_tags.map((tag, i) => (
-                    <Chip key={i} variant="outlined" size="small" label={tag} />
-                  ))}
-                </div>
-              </CardContent>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-    </div>
-  );
+	if (posts.length === 0) {
+		return (
+			<div className={styles.loading}>
+				<CircularProgress />
+			</div>
+		);
+	}
+	return (
+		<div className={styles.items}>
+			<div className={styles.container}>
+				<Grid container direction="row" justify="space-evenly" alignItems="center">
+					{posts.map((post) => (
+						<Grid item xs={12} md={5} component={Card} key={post.id} className={styles.card}>
+							<CardContent>
+								<Typography variant="h2">{post.post_title}</Typography>
+								<Divider component="li" />
+								<img className={styles.sectionimg} src={post.post_heroimage} alt="postimage" />
+								<Typography variant="body2" align="justify">
+									{post.post_shortext}
+								</Typography>
+								{post.post_website ? (
+									<MyButton href={post.post_website} target="noopener">
+										Project site
+									</MyButton>
+								) : null}
+
+								<MyButton href={post.post_github} target="noopener">
+									Github
+								</MyButton>
+								<div>
+									{post.post_tags.map((tag, i) => (
+										<Chip key={i} variant="outlined" size="small" label={tag} />
+									))}
+								</div>
+							</CardContent>
+						</Grid>
+					))}
+				</Grid>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectList;
